e3e739e748f8de316d73690562d76201b6a4835c
rlu132 <98561497+rlu132@users.noreply.github.com>
2024-09-13 15:35:33 -0700

    Added card draw functions

--- deck.py
+++ deck.py
@@ -1,9 +1,11 @@
+import random
+
 class deck():
     def __init__(self) -> None:
         self.deck = self.create_deck()
         print("deck created")
         
-    def create_deck(self) -> dict:
+    def create_deck(self) -> list:
         # dictionary to describe cards
         # [n, pos, atk, d, img]
         #  n -> Name of the card
@@ -17,23 +19,30 @@ class deck():
         #    - aoe ranges from x1 - x2, y1 - y2
         #  d -> short description of the card
         #  img -> image to be displayed on the player hand and queue
-        card_deck = dict()
+        
         # common cards
-        card_deck[len(card_deck)+1] = ["Move Left", [-1,0], None, "Moves Left", "some/file/path.png"]
-        card_deck[len(card_deck)+1] = ["Move Right", [1,0], None, "Moves right", "some/file/path.png"]
-        card_deck[len(card_deck)+1] = ["Move Up", [0,1], None, "Moves up", "some/file/path.png"]
-        card_deck[len(card_deck)+1] = ["Move Down", [0,-1], None, "Moves down", "some/file/path.png"]
-        card_deck[len(card_deck)+1] = ["flying kick", [2,1], [1,[0,5,-1,1]],"A kick whilst flying","some/file/s.png"]
-        card_deck[len(card_deck)+1] = ["Kick", None, [1,[1,0,False],[(1,1)]], "A kick aimed towards the family jewels", "some/file/path.png"]
+        common_deck = dict()
+        common_deck[len(common_deck)+1] = ["Move Left", [-1,0], None, "Moves Left", "some/file/path.png"]
+        common_deck[len(common_deck)+1] = ["Move Right", [1,0], None, "Moves right", "some/file/path.png"]
+        common_deck[len(common_deck)+1] = ["Move Up", [0,1], None, "Moves up", "some/file/path.png"]
+        common_deck[len(common_deck)+1] = ["Move Down", [0,-1], None, "Moves down", "some/file/path.png"]
+        common_deck[len(common_deck)+1] = ["flying kick", [2,1], [1,[0,5,-1,1]],"A kick whilst flying","some/file/s.png"]
+        common_deck[len(common_deck)+1] = ["Kick", None, [1,[1,0,False],[(1,1)]], "A kick aimed towards the family jewels", "some/file/path.png"]
         
         # rare cards
-        card_deck[len(card_deck)+1] = ["flying kick", [2,1], [1,[2,0,False],[-1,1,1,1]],"A kick whilst flying","some/file/s.png"]
-        card_deck[len(card_deck)+1] = ["Roundhouse Kick", None, [1,[0,5,-1,1]],"flying kick","some/file/s.png"]
+        rare_deck = dict()
+        rare_deck[len(rare_deck)+1] = ["flying kick", [2,1], [1,[2,0,False],[-1,1,1,1]],"A kick whilst flying","some/file/s.png"]
+        rare_deck[len(rare_deck)+1] = ["Roundhouse Kick", None, [1,[0,5,-1,1]],"flying kick","some/file/s.png"]
+        
         # epic cards
+        epic_deck = dict()
+        epic_deck[len(epic_deck)+1] = ["epic placeholder", [2,1], [1,[2,0,False],[-1,1,1,1]],"A kick whilst flying","some/file/s.png"]
 
         # legendary careds
+        leg_deck = dict()
 
-        return card_deck
+
+        return [common_deck,rare_deck, epic_deck, leg_deck]
 
     def get_card_name(self, id: int) -> str:
         return self.deck[id][0]
@@ -50,3 +59,52 @@ class deck():
     def get_card_img(self, id:int) -> str:
         return self.deck[id][4]
 
+    # takes in n number of cards to draw and time left in game, will return n sized list
+    def fill_hand(self, n:int, time:int) -> list:
+        cards_drawn = []
+        for i in range(n):
+            bucket_choice = random.randrange(1,100)
+            # before 3:30, chances for C/R/E/L -> 60/35/5/0
+            if time > 210:
+                if bucket_choice < 61:
+                    cards_drawn.append(self.draw_card(0))
+                elif bucket_choice < 96:
+                    cards_drawn.append(self.draw_card(1))
+                else:
+                    cards_drawn.append(self.draw_card(2))
+            # between 2:00 and 3:30 chances for C/R/E/L -> 45/40/10/1
+            elif time > 120:
+                if bucket_choice < 41:
+                    cards_drawn.append(self.draw_card(0))
+                elif bucket_choice < 71:
+                    cards_drawn.append(self.draw_card(1))
+                elif bucket_choice < 91:
+                    cards_drawn.append(self.draw_card(2))
+                else:
+                    cards_drawn.append(self.draw_card(3))
+            # between 2:00 and 3:30 chances for C/R/E/L -> 30/40/25/5
+            elif time > 60:
+                if bucket_choice < 31:
+                    cards_drawn.append(self.draw_card(0))
+                elif bucket_choice < 71:
+                    cards_drawn.append(self.draw_card(1))
+                elif bucket_choice < 96:
+                    cards_drawn.append(self.draw_card(2))
+                else:
+                    cards_drawn.append(self.draw_card(3))
+            # between 2:00 and 3:30 chances for C/R/E/L -> 15/30/45/10
+            else:
+                if bucket_choice < 16:
+                    cards_drawn.append(self.draw_card(0))
+                elif bucket_choice < 46:
+                    cards_drawn.append(self.draw_card(1))
+                elif bucket_choice < 91:
+                    cards_drawn.append(self.draw_card(2))
+                else:
+                    cards_drawn.append(self.draw_card(3))
+                
+        return cards_drawn
+
+    def draw_card(self, bucket:int) -> int:
+        return self.deck[bucket][random.randint(1,len(self.deck[bucket]))]
+        
