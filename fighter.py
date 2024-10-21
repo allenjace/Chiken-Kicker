3aa598038edee63afb20c8e3363cf36e9cfb5f53
rlu132 <98561497+rlu132@users.noreply.github.com>
2024-10-20 19:00:59 -0700

    Update fighter.py
    added card movement functionality

--- fighter.py
+++ fighter.py
@@ -2,11 +2,11 @@ import pygame
 from deck import *
 
 class Fighter():
-    def __init__(self, x, y, flip, data, sprite_sheet, animation_steps):
+    def __init__(self, x, y, flip, data, sprite_sheet, animation_steps, hand):
         self.size = data[0]
         self.image_scale = data[1]
         self.offset = data[2]
-        self.rect = pygame.Rect((x, y, 80, 100))
+        self.rect = self.orig_rect = pygame.Rect((x, y, 80, 100))
         self.flip = flip
         self.animation_list = self.load_images(sprite_sheet, animation_steps)
         self.action = 0 # 0: idle 1: run 2: jump 3: attack1 4:attack2 5: hit 6: death
@@ -19,8 +19,8 @@ class Fighter():
         self.attacking = False
         self.attack_type = 0
         self.health = 100
-        # self.playerhand = deck.fill_hand(5,300)
-        # self.playerqueue = queue.Queue()
+        self.playerhand = hand
+        self.playerqueue = queue.Queue()
 
     def load_images(self, sprite_sheet, animation_steps):
         # extract images
@@ -137,5 +137,52 @@ class Fighter():
 
     def draw(self, surface):
         img = pygame.transform.flip(self.image, self.flip, False)
-        pygame.draw.rect(surface, (255, 0 , 0), self.rect)
-        surface.blit(img, (self.rect.x - (self.offset[0] * self.image_scale), self.rect.y - (self.offset[1] * self.image_scale)))
+        pygame.draw.rect(surface, (0, 255 , 0), self.rect, 2)
+        # surface.blit(img, (self.rect.x - (self.offset[0] * self.image_scale), self.rect.y - (self.offset[1] * self.image_scale)))
+
+    def add_card(self, card):
+        self.playerqueue.put(card)
+        # print('placing and removing', card)
+        if card[2] != 0:
+            self.playerhand.remove(card)
+
+    def card_move(self, mvmt, target):
+        if self.flip:
+            self.rect.x -= mvmt[0]
+        else:
+            self.rect.x += mvmt[0]
+        self.rect.y += mvmt[1]
+
+        if mvmt[2] != self.image_scale:
+            if mvmt[2] < 1:
+                self.rect.scale_by(mvmt[2])
+                self.image_scale = mvmt[2]
+            elif self.image_scale < 1 and mvmt[2] >= 1:
+                self.rect.scale_by(1/self.image_scale)
+                self.image_scale(mvmt[2])
+                self.rect.scale_by(self.image_scale)
+        
+        if target.rect.centerx > self.rect.centerx:
+            self.flip = False
+        else: 
+            self.flip = True
+
+    def detect_col(self, opp, move):
+        if (self.rect.y + self.rect.h) > opp.rect.y and (opp.rect.y + opp.rect.h) > (self.rect.y + (self.rect.h/2)):
+            if (opp.rect.x < self.rect.x + move[0]) and opp.rect.x > self.rect.x + self.rect.w:
+                return True
+        return False
+    
+    def print_cards(self):
+        temp =[]
+        for i in self.playerhand:
+            temp.append(i)
+        print(temp)
+
+    def create_hitbox(self, action):
+        # creates a hitbox for the related move, will create on the side closest to 
+        if self.flip:
+            hitbox = pygame.Rect((self.rect.x - self.rect.w * action[5][0], self.rect.y + self.rect.h - self.rect.h * action[5][1],self.rect.w * action[5][0],self.rect.h * action[5][1]))
+        else:
+            hitbox = pygame.Rect((self.rect.x + self.rect.w, self.rect.y + self.rect.h - self.rect.h * action[5][1],self.rect.w * action[5][0],self.rect.h * action[5][1]))
+        return hitbox
